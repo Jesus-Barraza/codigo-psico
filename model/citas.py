81b5d76c988384741d1bd1d9b7c3a2de1dec4960
Barraza-Jesus <190627332+Jesus-Barraza@users.noreply.github.com>
2025-10-31 22:34:25 -0600

    feat: se añadio el archivo de citas

---
+++ model/citas.py
@@ -0,0 +1,25 @@
+class Citas():
+    def __init__ (self, id):
+        self._id=id
+
+    def agregarcita(self):
+        pass
+
+    def mostrarcita(self):
+        pass
+
+    def buscarcita(self):
+        pass
+
+    def borrarcita(self):
+        pass
+
+    def actualizarcita(self):
+        pass
+
+    def cambiarstatus(self):
+        pass
+
+    def avisartutor(self):
+        pass
+
