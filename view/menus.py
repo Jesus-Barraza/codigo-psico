936308ec4bec9d2cd8bd746feb63499aad0d3e95
Jesús Barraza <190627332+Jesus-Barraza@users.noreply.github.com>
2025-12-03 21:24:50 -0600

    Feat: diseño
    Se actualizó el diseño de la pantalla de inicio

--- view/menus.py
+++ view/menus.py
@@ -42,45 +42,121 @@ class Menu():
     def menuInicio(self, ventana):
         ventana.controls.clear()
         self.fontFamily(ventana)
-        container=ft.Container(
-            ft.Column(
-                [
-                    ft.Image(
-                        src="logo_psicopedagogia_v2.svg",
-                        width=200,
-                        height=200,
+        # Some Flet versions don't include Positioned; use Rows/Columns instead
+        top_row = ft.Row(
+            [
+                ft.Container(expand=True),
+                ft.Container(
+                    content=ft.Image(
+                        src="esquina2.svg",
+                        width=500,
+                        height=500,
                         fit=ft.ImageFit.CONTAIN,
                     ),
-                    ft.Text(
-                        "Bienvenido al Programa de\nPsicopedagogía",
-                        size=40,
-                        color="#034748",
-                        font_family="Jaques_Francois",
-                        text_align="center",
+                    margin=ft.margin.only(top=-200, right=-200),
+                    alignment=ft.alignment.top_right,
+                ),
+            ],
+            vertical_alignment=ft.CrossAxisAlignment.START,
+        )
+
+        bottom_row = ft.Row(
+            [
+                ft.Container(
+                    content=ft.Image(
+                        src="esquina1.svg",
+                        width=500,
+                        height=700,
+                        fit=ft.ImageFit.CONTAIN,
                     ),
-                    ft.ElevatedButton(
-                        text="Iniciar sesión",
-                        width=200,
-                        height=60,
-                        bgcolor="#D9D9D9",
-                        color="#000000",
-                        on_click=lambda:print("Iniciar sesión"),
-                    ),
-                    ft.ElevatedButton(
-                        text="Registrarse",
-                        width=200,
-                        height=60,
-                        bgcolor="#D9D9D9",
-                        color="#000000",
-                        on_click=lambda:print("Iniciar sesión"),
-                    ),
-                ],
-                alignment="center",
-                horizontal_alignment="center",
-            ),
+                    margin=ft.margin.only(left=-200, bottom=-300),
+                    alignment=ft.alignment.bottom_left,
+                ),
+                ft.Container(expand=True),
+            ]
+            ,
+            vertical_alignment=ft.CrossAxisAlignment.END,
+        )
+
+        main_column = ft.Column(
+            [
+                ft.Container(
+                    ft.Column([
+                        ft.Image(
+                            src="logo_psicopedagogia_v2.svg",
+                            width=200,
+                            height=200,
+                            fit=ft.ImageFit.CONTAIN,
+                        ),
+                        ft.Text(
+                            "Bienvenido al programa de\nPsicopedagogía",
+                            size=40,
+                            color="#034748",
+                            font_family="Jaques_Francois",
+                            text_align="center",
+                            weight="w900",
+                        ),
+                    ], horizontal_alignment=ft.CrossAxisAlignment.CENTER),
+                    alignment=ft.alignment.center,
+                ),
+                ft.Container(
+                    ft.Column([
+                        ft.ElevatedButton(
+                            text="Iniciar sesión",
+                            width=200,
+                            height=60,
+                            bgcolor="#D9D9D9",
+                            color="#000000",
+                            on_click=lambda e: self.menuIniciarSesion(ventana),
+                            style=ft.ButtonStyle(
+                                shape=ft.RoundedRectangleBorder(radius=12),
+                                side=ft.BorderSide(1, "#000000"),
+                                text_style=ft.TextStyle(
+                                    weight="w400",
+                                    font_family="Jaques_Francois",
+                                    size=20,
+                                ),
+                            ),
+                        ),
+                        ft.ElevatedButton(
+                            text="Registrarse",
+                            width=200,
+                            height=60,
+                            bgcolor="#D9D9D9",
+                            color="#000000",
+                            on_click=lambda e: self.menuRegistrarse(ventana),
+                            style=ft.ButtonStyle(
+                                shape=ft.RoundedRectangleBorder(radius=12),
+                                side=ft.BorderSide(1, "#000000"),
+                                text_style=ft.TextStyle(
+                                    weight="w400",
+                                    font_family="Jaques_Francois",
+                                    size=20,
+                                ),
+                            ),
+                        ),
+                    ], horizontal_alignment=ft.CrossAxisAlignment.CENTER),
+                    alignment=ft.alignment.center,
+                ),
+            ],
+            alignment=ft.MainAxisAlignment.SPACE_EVENLY,
+            horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+            expand=True,
+        )
+
+        container = ft.Container(
+            content=ft.Column([top_row, main_column, bottom_row], expand=True),
             width=1920,
             height=1080,
-
+            bgcolor="#BCDFE6",
         )
         ventana.add(container)
+        ventana.update()
+    
+    def menuIniciarSesion(self, ventana):
+        ventana.controls.clear()
+        ventana.update()
+
+    def menuRegistrarse(self, ventana):
+        ventana.controls.clear()
         ventana.update()
