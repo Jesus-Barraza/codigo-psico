fe9b5be09515985150fca99b13fff75ca7ee8e71
Jesús Barraza <190627332+Jesus-Barraza@users.noreply.github.com>
2025-12-03 22:46:12 -0600

    datos

--- main.py
+++ main.py
@@ -1,107 +1,16 @@
 '''
-Este es un archivo temporal en lo que se realizan las demás instancias.
-Por lo pronto esto representarán los acomodos exactos del menú y funciones básicas
+Este es el archivo de main. En este archivo se ingresará directamente al directorio e interactuar con el resto.
 '''
+import flet as ft
+from view import menus
 
-import os
+#Clase para iniciar la aplicación
+class app():
+    def __init__(self, ventana):
+        menus.Menu(ventana)
 
-class App():
-    def __init__(self):
-        self.menuInicioSesion()
-    
-    @staticmethod
-    def borrarPantalla():
-        os.system("cls")
-
-    @staticmethod
-    def esperarTecla():
-        input("Presiona enter para continuar")
-
-    def menuInicioSesion(self):
-        self.borrarPantalla()
-        opc=True
-        while opc:
-            print("inicio de sesion y contraseña")
-            opc=input("Elije tu opción \n1- Inicio de sesión \n2- Registro \n3- Salir \n (1-3):").strip()
-            match opc:
-                case "1":
-                    self.borrarPantalla()
-                    self.inicio_sesion()
-                case "2":
-                    self.borrarPantalla()
-                    self.registrar()
-                case "3":
-                    opc=False
-                    print("Terminó la ejecución del sistema")
-                case _:
-                    opc=True
-                    self.borrarPantalla()
-                    print("Opción no válida")
-
-    def inicio_sesion(self):
-        print("inicio de sesion")
-        correo=input("correo electronico: ").strip
-        contra=input("contraseña: ").strip
-        if correo=="nosexd@gmail.com" and contra=="1234":
-            self.main()
-
-    def registrar(self):
-        nombre=input("Ingrese su nombre")
-        correo=input("Ingrese el correo electrónico")
-        contraseña=input("Ingrese su contraseña")
-        self.main()
-
-    def menuCitas(self):
-        print("Menu de citas")
-        print(f"| {'nombre del estudiante':<50} | {'fecha':<20} | {'prioridad':10} | {'n° de cita':<20} | {'status':<50}")
-        print("no hay citas por el momento")
-        self.esperarTecla()
-        self.main()
-
-    def calendario(self):
-        print("calendario de citas")
-        print("No hay citas registradas por el momento")
-        self.esperarTecla()
-        self.main()
-
-    def Notificaciones(self):
-        print("Menú de notificaciones")
-        print("No hay notificaciones nuevas por el momento")
-        self.esperarTecla()
-        self.main()
-
-    def configuracion(self):
-        print("configuracion")
-        print("Nota de creador: el menú no es funcional dado a que no hay funciones importantes que mencionar")
-        self.esperarTecla()
-        self.main()
-    
-    def main(self):
-        self.borrarPantalla()
-        opc=True
-        while opc:
-            print("Menú inicial")
-            opc=input("Elige una opcion: \n1.-Menú de citas \n2.-Calendario \n3.-Notificaciones \n4.-Configuración \n5.-Salir \n (1-5):").strip()
-            match opc:
-                case "1":
-                    self.borrarPantalla()
-                    self.menuCitas()
-                case "2":
-                    self.borrarPantalla()
-                    self.calendario()
-                case "3":
-                    self.borrarPantalla()
-                    self.Notificaciones()
-                case "4":
-                    self.borrarPantalla()
-                    self.configuracion()
-                case "5":
-                    print("Volviendo al inicio de sesion")
-                    self.menuInicioSesion()
-                case "_":
-                    opc=True
-                    self.borrarPantalla()
-                    print("Opción no válida, inténtelo de nuevo")
-
+#Punto de entrada principal
 if __name__ == "__main__":
-    a=App()
+    ft.app(target=app, assets_dir="img")
+    
+
---
+++ view/menus.py
@@ -0,0 +1,248 @@
+import flet as ft
+from threading import Timer
+
+#Clase para el menú principal
+class Menu():
+    #Configuración inicial de la ventana
+    def __init__(self, ventana):
+        ventana.title = "Programa de Psicopedagogía" #Título
+        ventana.theme_mode = ft.ThemeMode.LIGHT #Modo de tema claro
+        ventana.vertical_alignment = "center"
+        ventana.horizontal_alignment = "center" #Alineación centrada
+        ventana.resizable = False #No redimensionable
+        ventana.window_width = 1920
+        ventana.window_height = 1080 #Tamaño de ventana
+        ventana.bgcolor = "#BCDFE6" #Color de fondo
+        ventana.update() #Actualizar ventana
+        self.pantallaCarga(ventana) 
+
+    #Fuentes de texto como función
+    @staticmethod
+    def fontFamily(ventana):
+        ventana.fonts = {
+            "Jaques_Francois": "fonts/jaques_francois/JacquesFrancois-Regular.ttf"
+        }
+
+    #Pantalla de carga inicial
+    def pantallaCarga(self, ventana):
+        #Limpiar controles y mostrar imagen de carga
+        ventana.controls.clear()
+        imagen = ft.Image(
+            src="logo_psicopedagogia_v2.svg",
+            width=600,
+            height=600,
+            fit=ft.ImageFit.CONTAIN,
+        )
+        ventana.add(imagen)
+        ventana.update()
+        #Temporizador para cambiar a menú de inicio
+        start=Timer(3.0, lambda:self.menuInicio(ventana))
+        start.start()
+
+    def menuInicio(self, ventana):
+        ventana.controls.clear()
+        self.fontFamily(ventana)
+        # Some Flet versions don't include Positioned; use Rows/Columns instead
+        top_row = ft.Row(
+            [
+                ft.Container(expand=True),
+                ft.Container(
+                    content=ft.Image(
+                        src="esquina2.svg",
+                        width=500,
+                        height=500,
+                        fit=ft.ImageFit.CONTAIN,
+                    ),
+                    margin=ft.margin.only(top=-200, right=-200),
+                    alignment=ft.alignment.top_right,
+                ),
+            ],
+            vertical_alignment=ft.CrossAxisAlignment.START,
+        )
+
+        main_column = ft.Column(
+            [
+                ft.Container(
+                    ft.Column([
+                        ft.Image(
+                            src="logo_psicopedagogia_v2.svg",
+                            width=200,
+                            height=200,
+                            fit=ft.ImageFit.CONTAIN,
+                        ),
+                        ft.Text(
+                            "Bienvenido al programa de\nPsicopedagogía",
+                            size=40,
+                            color="#034748",
+                            font_family="Jaques_Francois",
+                            text_align="center",
+                            weight="w900",
+                        ),
+                    ], horizontal_alignment=ft.CrossAxisAlignment.CENTER),
+                    alignment=ft.alignment.center,
+                ),
+                ft.Container(
+                    ft.Column([
+                        ft.ElevatedButton(
+                            text="Iniciar sesión",
+                            width=200,
+                            height=60,
+                            bgcolor="#D9D9D9",
+                            color="#000000",
+                            on_click=lambda e: self.menuIniciarSesion(ventana),
+                            style=ft.ButtonStyle(
+                                shape=ft.RoundedRectangleBorder(radius=12),
+                                side=ft.BorderSide(1, "#000000"),
+                                text_style=ft.TextStyle(
+                                    weight="w400",
+                                    font_family="Jaques_Francois",
+                                    size=20,
+                                ),
+                            ),
+                        ),
+                        ft.ElevatedButton(
+                            text="Registrarse",
+                            width=200,
+                            height=60,
+                            bgcolor="#D9D9D9",
+                            color="#000000",
+                            on_click=lambda e: self.menuRegistrarse(ventana),
+                            style=ft.ButtonStyle(
+                                shape=ft.RoundedRectangleBorder(radius=12),
+                                side=ft.BorderSide(1, "#000000"),
+                                text_style=ft.TextStyle(
+                                    weight="w400",
+                                    font_family="Jaques_Francois",
+                                    size=20,
+                                ),
+                            ),
+                        ),
+                    ], horizontal_alignment=ft.CrossAxisAlignment.CENTER),
+                    alignment=ft.alignment.center,
+                ),
+            ],
+            alignment=ft.MainAxisAlignment.SPACE_EVENLY,
+            horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+            expand=True,
+        )
+
+        bottom_row = ft.Row(
+            [
+                ft.Container(
+                    content=ft.Image(
+                        src="esquina1.svg",
+                        width=500,
+                        height=700,
+                        fit=ft.ImageFit.CONTAIN,
+                    ),
+                    margin=(-200, 0, -100, -200),
+                    alignment=ft.alignment.bottom_left,
+                ),
+                ft.Container(expand=True),
+            ],
+            vertical_alignment="end",
+            height=0,
+        )
+
+        container = ft.Container(
+            content=ft.Column([top_row, main_column, bottom_row], expand=True),
+            width=1920,
+            height=1080,
+            bgcolor="#BCDFE6",
+        )
+        ventana.add(container)
+        ventana.update()
+    
+    def menuIniciarSesion(self, ventana):
+        ventana.controls.clear()
+        ventana.update()
+
+    def menuRegistrarse(self, ventana):
+        ventana.controls.clear()
+        self.fontFamily(ventana)
+        top_row = ft.Row(
+            [
+                ft.Container(expand=True),
+                ft.Container(
+                    content=ft.Image(
+                        src="esquina2.svg",
+                        width=500,
+                        height=500,
+                        fit=ft.ImageFit.CONTAIN,
+                    ),
+                    margin=ft.margin.only(top=-200, right=-200),
+                    alignment=ft.alignment.top_right,
+                ),
+            ],
+            vertical_alignment=ft.CrossAxisAlignment.START,
+        )
+
+        main_column = ft.Column(
+            [
+                ft.Container(
+                    ft.Column([
+                        ft.Text(
+                            "Registrar nuevo usuario",
+                            size=40,
+                            color="#034748",
+                            font_family="Jaques_Francois",
+                            text_align="center",
+                            weight="w900",
+                        ),
+                    ], horizontal_alignment=ft.CrossAxisAlignment.CENTER),
+                    alignment=ft.alignment.center,
+                ),
+                ft.Container(
+                    ft.Column([
+                        ft.ElevatedButton(
+                            text="Volver",
+                            width=200,
+                            height=60,
+                            bgcolor="#D9D9D9",
+                            color="#000000",
+                            on_click=lambda e: self.menuInicio(ventana),
+                            style=ft.ButtonStyle(
+                                shape=ft.RoundedRectangleBorder(radius=12),
+                                side=ft.BorderSide(1, "#000000"),
+                                text_style=ft.TextStyle(
+                                    weight="w400",
+                                    font_family="Jaques_Francois",
+                                    size=20,
+                                ),
+                            ),
+                        ),
+                    ], horizontal_alignment=ft.CrossAxisAlignment.CENTER),
+                    alignment=ft.alignment.center,
+                ),
+            ],
+            alignment=ft.MainAxisAlignment.SPACE_EVENLY,
+            horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+            expand=True,
+        )
+
+        bottom_row = ft.Row(
+            [
+                ft.Container(
+                    content=ft.Image(
+                        src="esquina1.svg",
+                        width=500,
+                        height=700,
+                        fit=ft.ImageFit.CONTAIN,
+                    ),
+                    margin=(-200, 0, -100, -200),
+                    alignment=ft.alignment.bottom_left,
+                ),
+                ft.Container(expand=True),
+            ],
+            vertical_alignment="end",
+            height=0,
+        )
+
+        container = ft.Container(
+            content=ft.Column([top_row, main_column, bottom_row], expand=True),
+            width=1920,
+            height=1080,
+            bgcolor="#BCDFE6",
+        )
+        ventana.add(container)
+        ventana.update()
